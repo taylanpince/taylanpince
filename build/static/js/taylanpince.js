4a1dcf0c76f38808cabfef66ae653c57399ff18f
Taylan Pince <taylanpince@gmail.com>
2008-06-20 13:13:06 -0400

    Added the footer styles, a new hr style, rss links, some form styles, etc.

---
+++ build/static/js/taylanpince.js
@@ -0,0 +1,54 @@
+/*
+*	Taylan Pince
+*	Utility functions for taylanpince.com
+*	
+*	Requires jQuery library (http://www.jquery.com)
+*	
+*	Taylan Pince (taylanpince@gmail.com) - June 20, 2008
+*/
+
+$.extend($.namespace("core.TaylanPince"), {
+    
+    init_markers : function() {
+		$("html").addClass("has-js");
+		
+		$("li:last-child").addClass("last-child");
+		$("li:first-child").addClass("first-child");
+        
+		$("input[@type=text]").addClass("text");
+		$("input[@type=submit]").addClass("submit");
+		$("input[@type=password]").addClass("text");
+		$("input[@type=file]").addClass("file");
+		$("input[@type=radio]").addClass("radio");
+		$("input[@type=checkbox]").addClass("checkbox");
+		$("input[@type=image]").addClass("image");
+        
+		$("hr").wrap('<div class="hr"></div>');
+	},
+	
+	header_call : "",
+	
+	init_header : function() {
+	    this.header_call = $("#HeaderCall").html();
+	    
+	    $("#Header").find("a").hover(function() {
+	        $("#HeaderCall").animate({"opacity": 0}, {"complete": function() {
+	            $("#HeaderCall").html('home <span class="arrow">&raquo;</a>').animate({"opacity": 1}, {"duration": "normal", "queue": true});
+	        }, "duration": "normal", "queue": true});
+	    }, function() {
+	        $("#HeaderCall").animate({"opacity": 0}, {"complete": function() {
+	            $("#HeaderCall").html(core.TaylanPince.header_call).animate({"opacity": 1}, {"duration": "normal", "queue": true});
+	        }, "duration": "normal", "queue": true});
+	    });
+	},
+    
+    init : function() {
+        this.init_markers();
+        this.init_header();
+    }
+    
+});
+
+$(function() {
+    core.TaylanPince.init();
+});
