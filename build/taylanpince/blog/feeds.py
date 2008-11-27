7e045b8d405003b291cf2ea06cf8754811229aee
Taylan Pince <taylanpince@gmail.com>
2008-11-26 21:31:51 -0500

    Updated RSS feed with some additional info - converted to ATOM.

--- build/taylanpince/blog/feeds.py
+++ build/taylanpince/blog/feeds.py
@@ -1,5 +1,8 @@
 from django.core.urlresolvers import reverse
+from django.utils.feedgenerator import Atom1Feed
 from django.contrib.syndication.feeds import Feed
+
+from tagging.models import Tag
 
 from blog.models import Category, Post
 
@@ -8,29 +11,34 @@ class RssLatestPosts(Feed):
     title_template = "feeds/post_title.html"
     description_template = "feeds/post_description.html"
     
+    feed_type = Atom1Feed
+    
     title = "Taylan Pince - Blog Posts"
+    subtitle = "Latest blog posts from taylanpince.com"
+    author_name = "Taylan Pince"
+    copyright = "Copyright (c) 2008, Taylan Pince"
     
     def link(self):
         return reverse("blog_landing")
     
-    description = "Latest blog posts from taylanpince.com"
-    
     def items(self):
         return Post.objects.all()[:15]
+    
+    def item_pubdate(self, item):
+        return item.creation_date
+    
+    def item_categories(self, item):
+        return [t.name for t in Tag.objects.get_for_object(item)]
 
 
-class RssLatestPostsByCategory(Feed):
-    title_template = "feeds/post_title.html"
-    description_template = "feeds/post_description.html"
-    
-    def get_object(self, bits):
-        if len(bits) != 1:
-            raise ObjectDoesNotExist
-        
-        return Category.objects.get(slug__exact=bits[0])
-    
+class RssLatestPostsByCategory(RssLatestPosts):
     def title(self, obj):
         return "Taylan Pince - Blogs Posts Listed Under %s" % obj.title
+    
+    def description(self, obj):
+        return "Latest blog posts listed under %(category)s from taylanpince.com" % {
+            "category": obj.title,
+        }
     
     def link(self, obj):
         if not obj:
@@ -40,10 +48,11 @@ class RssLatestPostsByCategory(Feed):
             "slug": obj.slug,
         })
     
-    def description(self, obj):
-        return "Latest blog posts listed under %(category)s from taylanpince.com" % {
-            "category": obj.title,
-        }
+    def get_object(self, bits):
+        if len(bits) != 1:
+            raise ObjectDoesNotExist
+        
+        return Category.objects.get(slug__exact=bits[0])
     
     def items(self, obj):
         return Post.objects.filter(categories=obj)[:15]
