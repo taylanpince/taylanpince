279a336a3207b985b1e55181f48e1e3eb58dc2b0
Taylan Pince <taylanpince@gmail.com>
2008-12-12 10:02:02 +0200

    Updated the Akismet SPAM check signal to only process comments that are not published.

--- build/taylanpince/blog/signals.py
+++ build/taylanpince/blog/signals.py
@@ -11,22 +11,23 @@ def moderate_comment(sender, instance, **kwargs):
     """
     Connects to the Akismet API to verify the comment's authenticity
     """
-    akismet_api = Akismet(key=settings.AKISMET_API_KEY, blog_url="http://%s/" % Site.objects.get_current().domain)
+    if not instance.published:
+        akismet_api = Akismet(key=settings.AKISMET_API_KEY, blog_url="http://%s/" % Site.objects.get_current().domain)
     
-    if akismet_api.verify_key():
-        try:
-            if not akismet_api.comment_check(smart_str(instance.body), data={
-                "user_ip": instance.ip_address,
-                "user_agent": "",
-                "referrer": "unknown",
-                "comment_type": "comment",
-                "comment_author": instance.author,
-                "comment_author_email": instance.email,
-                "comment_author_url": instance.url,
-            }, build_data=True):
-                instance.published = True
-        except:
-            pass
+        if akismet_api.verify_key():
+            try:
+                if not akismet_api.comment_check(smart_str(instance.body), data={
+                    "user_ip": instance.ip_address,
+                    "user_agent": "",
+                    "referrer": "unknown",
+                    "comment_type": "comment",
+                    "comment_author": instance.author,
+                    "comment_author_email": instance.email,
+                    "comment_author_url": instance.url,
+                }, build_data=True):
+                    instance.published = True
+            except:
+                pass
 
 
 def ping_blog_indexes(sender, instance, created, **kwargs):
